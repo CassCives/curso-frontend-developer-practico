a386c0961228ae63da01cacf55cfe322c87ebac7
Cassius Cives Muñoz <cass-cives@hotmail.com>
2022-09-07 12:55:45 -0300

    Modulo 20

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const hamburguesaMenu = document.querySelector(".menu");
+const carritoMenu = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 hamburguesaMenu.addEventListener("click", toggleMobileMenu);
+carritoMenu.addEventListener("click", togglecarritoMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
+}
+function togglecarritoMenu() {
+    aside.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
 }
