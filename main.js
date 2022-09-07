1395abb5accc391fd3f042c9668b78d53d5f74d6
Cassius Cives Muñoz <cass-cives@hotmail.com>
2022-09-07 12:38:07 -0300

    Modulo 20

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const hamburguesaMenu = document.querySelector(".menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+hamburguesaMenu.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
